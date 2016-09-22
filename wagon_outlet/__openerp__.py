335b212c968cee11b744a745ac802337f456b63f
AfroMonkey <moisalejandro@gmail.com>
2016-09-22 16:41:31 -0500

    [ADD] wagon_outlet

---
+++ wagon_outlet/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Wagon Outlet',
+    'version': '1.0',
+    'author': 'Humanytek',
+    'website': 'http://humanytek.com',
+    'depends': ['wagon', 'vehicle_outlet'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/wagon_outlet_access_rules.xml',
+        'views/sale_order.xml',
+        'views/wagon_outlet.xml',
+    ]
+}
---
+++ wagon_outlet/models/__init__.py
@@ -0,0 +1,2 @@
+from . import wagon_outlet
+from . import sale_order
